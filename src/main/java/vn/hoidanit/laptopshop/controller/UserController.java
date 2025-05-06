d198688803686364158fff8a95c36dd88c98cbb8
conqdinh94 <trancongdinh094@gmail.com>
2025-05-07 02:18:43 +0700

    add UserController

---
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -0,0 +1,35 @@
+package vn.hoidanit.laptopshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import vn.hoidanit.laptopshop.service.UserService;
+
+//MVC
+// @Controller
+// public class UserController {
+
+//     @RequestMapping("/")
+//     public String getHomePage() {
+//         return "Hello from controller";
+//     }
+
+// }
+
+@RestController
+public class UserController {
+
+    // DI: dependency injection
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping("/")
+    public String getHomePage() {
+        return userService.handleHello();
+    }
+
+}
